607f166f0859857304b4080e68dc4c32fcdf441d
DiegoDev <pentium4jsphy@gmail.com>
2025-05-16 21:15:33 -0600

    Mejora en la movilidad del personaje

--- main.py
+++ main.py
@@ -35,13 +35,13 @@ class Player(pygame.sprite.Sprite):
         self.energy = 100
 
     def update(self, keys):
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.rect.x -= PLAYER_SPEED
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.rect.x += PLAYER_SPEED
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.rect.y -= PLAYER_SPEED
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             self.rect.y += PLAYER_SPEED
         self.rect.clamp_ip(screen.get_rect())
 
